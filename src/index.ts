a6bc1e5ac55652e9f5ba691e5d2d064242cd5c76
hunghg255 <giahunghust@gmail.com>
2025-02-20 19:29:59 +0700

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// Main entrypoint for the bundler (ESBuild/Webpack/etc.) when creating the bundled
+// portion of a release.
+
+
+// import '../styles';
+import Editor from './draw/Editor';
+// export * from '../lib';
+
+export default Editor;

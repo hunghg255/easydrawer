37cc977b87d4e48c37d469e9db1c9368b854b57a
hunghg255 <giahunghust@gmail.com>
2025-03-14 12:03:02 +0700

    chore: update bundle global

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,10 @@
+import fs from 'node:fs';
+
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
+  format: ['esm', 'cjs', 'iife'],
   treeshake: true,
   clean: true,
   platform: 'browser',
@@ -11,4 +13,8 @@ export default defineConfig({
   minify: true,
   sourcemap: process.env.NODE_ENV !== 'production',
   ignoreWatch: ['**/node_modules', '**/dist'],
+  globalName: 'EasyDrawer',
+  onSuccess: async () => {
+    fs.renameSync('dist/index.global.js', 'dist/easydrawer.js');
+  }
 });
